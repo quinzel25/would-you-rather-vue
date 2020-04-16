2bdbb09ac26271de5a4369fbe517f2f8e339d77a
quinz <quinzel0325@gmail.com>
2020-04-16 17:45:50 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,110 @@
+<template>
+  <!--  main template for the page-->
+  <div id="app">
+        <div style="background-color: #f76a8c" class="jumbotron">
+            <h1>Would you rather...</h1>
+        </div>
+
+        <!--    this adds the template from the vue component-->
+<!--        this grabs the $emit data from child component and calls the method we are using-->
+        <would-you-rather-question
+        v-on:answer-change="answerChange"></would-you-rather-question>
+<!--      the results part of the page-->
+      <div id="results">
+            <h2>You would rather...</h2>
+          <ul id="result-list">
+
+<!--                v-show so that if it's true it will display list items-->
+              <li v-show="answerOne">{{answerOne }} <br></li>
+              <li v-show="answerTwo">{{answerTwo}} <br></li>
+              <li v-show="answerThree">{{answerThree}}</li>
+          </ul>
+
+      </div>
+
+  </div>
+</template>
+
+
+
+
+<script>
+
+
+  import WouldYouRatherQuestion from "../../would-you-rather-vue/src/components/WouldYouRatherQuestion";
+
+  export default {
+    name: 'App',
+    data() {
+      return {
+          // these are the variables that display our result set
+          answerOne: '',
+          answerTwo: '',
+          answerThree: '',
+      }
+    },
+    components: {
+      //grab this from import
+      WouldYouRatherQuestion
+    },
+    methods: {
+        // method here
+        // 'choice' is the data the child emitted to parent
+        answerChange(choice) {
+            // Probably not the best way to do this part but hey it works ¯\_(ツ)_/¯
+
+            //first question
+            let testString1 = 'be an author of a popular book'
+            let testString2 = 'be a musician with a popular album'
+            if (choice === testString1 || choice === testString2) {
+                this.answerOne = choice
+
+            }
+            // second question
+            let testString3 = 'live by a lot of trees'
+            let testString4 = 'live by the ocean'
+
+            if (choice === testString3 || choice === testString4) {
+                this.answerTwo = choice
+
+            }
+            // third question
+            let testString5 = 'eat a whole raw onion'
+            let testString6 = 'eat a whole lemon'
+
+            if (choice === testString5 || choice === testString6) {
+                this.answerThree = choice
+
+            }
+        }
+
+    }
+  }
+
+
+</script>
+
+<style>
+    #app {
+        margin: 40px 0 0;
+        text-align: center;
+
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+    body {
+        background-color: #c6ead9;
+    }
+
+    /*.jumbotron {*/
+    /*    background-color: #215c41;*/
+    /*    text-emphasis-color: #42b983;*/
+    /*}*/
+
+</style>
---
+++ src/components/WouldYouRatherQuestion.vue
@@ -0,0 +1,70 @@
+<template>
+
+<!--template for the html -->
+    <div class="question-container">
+<!--        a list element that will be re-used for each item in the questions object array-->
+        <ul id="question-list">
+<!--            v-for does the work for us-->
+            <li v-for="q in questions" :key="q.question" id="line">
+                <h2>{{q.question}}</h2>
+<!--                q.id makes sure that we can only select one radio button per question-->
+                <input  type="radio"  v-bind:value="q.answer1" v-model="q.id" v-on:change="$emit('answer-change', q.id)">
+                <label id="label1">{{q.answer1}}</label>
+                <input  type="radio"  v-bind:value="q.answer2"  v-model="q.id" v-on:change="$emit('answer-change', q.id)">
+                <label id="label2">{{q.answer2}}</label>
+            </li>
+
+        </ul>
+
+    </div>
+
+
+
+</template>
+<!--script-->
+<script>
+    export default {
+        name: "WouldYouRatherQuestion",
+        data() {
+            // array of our data
+            return {
+                questions: [
+                    {id: 0,
+                    question: '...be the author of a popular book or a musician in a band who released a popular album?',
+                    answer1: 'be an author of a popular book',
+                    answer2: 'be a musician with a popular album'
+                    },
+                    {id: 1,
+                        question: '...live in a place with a lot of trees or live in a place near the ocean?',
+                        answer1: 'live by a lot of trees',
+                        answer2: 'live by the ocean'
+                    },
+                    {id: 2,
+                        question: '...eat a whole raw onion or a whole lemon?',
+                        answer1: 'eat a whole raw onion',
+                        answer2: 'eat a whole lemon'
+                    }
+
+                ],
+
+
+
+            }
+        },
+
+    }
+</script>
+
+<style>
+#line {
+    margin: 20px;
+    border: 10px;
+    background-color: #f8dc88;
+
+
+}
+   #label1,#label2 {
+       font-size: 16px;
+
+   }
+</style>
